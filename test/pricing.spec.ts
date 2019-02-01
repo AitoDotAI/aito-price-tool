e9366d60314e0f2cad4fcf8f39b4d47785117a11
Kai Inkinen <kai@aito.ai>
2019-02-01 14:34:15 +0200

    Initial test for the pricing module

---
+++ test/pricing.spec.ts
@@ -0,0 +1,12 @@
+import { expect } from 'chai'
+import 'mocha'
+
+import { calculatePrice } from '../src/index'
+
+describe('Pricing module', () => {
+
+  it('should give prize 0 for empty array', () => {
+    expect(calculatePrice([])).to.equal(0)
+  })
+
+});
