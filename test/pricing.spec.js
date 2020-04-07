6b129c81e3862fdb3e0deb8a589fc8fb28c0adf3
Kai Inkinen <kai@aito.ai>
2020-04-07 12:41:23 +0300

    Few more tests and beautified the files

--- test/pricing.spec.js
+++ test/pricing.spec.js
@@ -1,4 +1,7 @@
-import { expect, fail } from 'chai'
+import {
+  expect,
+  fail
+} from 'chai'
 import 'mocha'
 
 import * as Calculator from '../dist/src/index'
@@ -47,5 +50,11 @@ describe('Pricing module', () => {
   it('should calculate prices for combined products', () => {
     expect(Calculator.calculatePrice(['PRODUCTION', 'PLUS_ONE_GB'], 1)).to.equal(24900 + 4900)
     expect(Calculator.calculatePrice(['PRODUCTION', 'DEVELOPER'], 1)).to.equal(24900 + 2900)
+  })
+
+  it('should calculate prices for random ordered combined products', () => {
+    expect(Calculator.calculatePrice(['PLUS_ONE_GB', 'PRODUCTION', 'PLUS_ONE_GB', 'DEVELOPER', 'SANDBOX'], 1)).to.be.greaterThan(0)
+    expect(Calculator.calculatePrice(['SANDBOX', 'DEVELOPER', 'DEVELOPER'], 1)).to.be.greaterThan(0)
+    expect(Calculator.calculatePrice(['PLUS_ONE_GB', 'PRODUCTION', 'PRODUCTION', 'PRODUCTION', 'PRODUCTION'], 1)).to.be.greaterThan(0)
   })
 })
--- test/pricing.spec.ts
+++ test/pricing.spec.ts
@@ -1,4 +1,7 @@
-import { assert, expect } from 'chai'
+import {
+  assert,
+  expect
+} from 'chai'
 import 'mocha'
 
 import * as Calculator from '../src/index'
@@ -57,5 +60,17 @@ describe('Pricing module', () => {
   it('should calculate prices for combined products', () => {
     expect(Calculator.calculatePrice(['PRODUCTION', 'PLUS_ONE_GB'], 1)).to.equal(24900 + 4900)
     expect(Calculator.calculatePrice(['PRODUCTION', 'DEVELOPER'], 1)).to.equal(24900 + 2900)
+  })
+
+  it('should calculate prices for random ordered combined products', () => {
+    expect(Calculator.calculatePrice(
+      ['PLUS_ONE_GB', 'PRODUCTION', 'PLUS_ONE_GB', 'DEVELOPER', 'SANDBOX'], 1)).to.be.greaterThan(0)
+
+    expect(Calculator.calculatePrice(
+      ['SANDBOX', 'DEVELOPER', 'DEVELOPER'], 1)).to.be.greaterThan(0)
+
+    expect(Calculator.calculatePrice(
+      ['PLUS_ONE_GB', 'PRODUCTION', 'PRODUCTION', 'PRODUCTION', 'PRODUCTION'],
+      1)).to.be.greaterThan(0)
   })
 })
